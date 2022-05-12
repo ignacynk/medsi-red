face8b1957114d0ce1ba00fe7072582431121826
ignacynk <ignacy.nkonopka@gmail.com>
2022-05-12 13:38:36 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/**
+  This CodeSandbox has been automatically generated using
+  `codesandboxer`. If you're curious how that happened, you can
+  check out our docs here: https://github.com/codesandbox/codesandboxer
+
+  If you experience any struggles with this sandbox, please raise an issue
+  on github. :)
+*/
+import React from "react";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./example";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
